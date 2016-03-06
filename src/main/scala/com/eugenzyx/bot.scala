3bd67db69a084f9a5ff06290051c1d35ebe77741
Eugen Prokhorenko <ievgen.prokhorenko@onapp.com>
2016-03-06 22:51:08 +0200

    fix /hey command

--- src/main/scala/com/eugenzyx/bot.scala
+++ src/main/scala/com/eugenzyx/bot.scala
@@ -8,7 +8,7 @@ import info.mukel.telegram.bots._
 object Bot extends TelegramBot(Utils.tokenFromFile("./bot.token")) with Polling with Commands {
   on(G.command)       { (sender, args) => replyTo(sender) { G.handler(sender, args) } }
   on(Help.command)    { (sender, args) => replyTo(sender) { Help.handler(sender, args) } }
-  on(Hey.command)     { (sender, args) => replyTo(sender) { Help.handler(sender, args) } }
+  on(Hey.command)     { (sender, args) => replyTo(sender) { Hey.handler(sender, args) } }
   on(Man.command)     { (sender, args) => replyTo(sender) { Man.handler(sender, args) } }
   on(Weather.command) { (sender, args) => replyTo(sender) { Weather.handler(sender, args) } }
   on(Photo.command)   { (sender, args) => Future { Photo.handler(sender, args)((photo, title) => sendPhoto(sender, photo, title), message => sendMessage(sender, message)) } }
