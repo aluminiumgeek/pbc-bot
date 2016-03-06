d59954ee63be457efa8946c0c86c8d1a4b98a16c
Eugen Prokhorenko <ievgen.prokhorenko@onapp.com>
2016-03-06 15:56:18 +0200

    load photos asynchronously

--- src/main/scala/com/eugenzyx/bot.scala
+++ src/main/scala/com/eugenzyx/bot.scala
@@ -1,5 +1,7 @@
 package com.eugenzyx
 
+import scala.concurrent._
+import scala.concurrent.ExecutionContext.Implicits.global
 import com.eugenzyx.commands._
 import info.mukel.telegram.bots._
 
@@ -9,5 +11,5 @@ object Bot extends TelegramBot(Utils.tokenFromFile("./bot.token")) with Polling 
   on(Hey.command)     { (sender, args) => replyTo(sender) { Help.handler(sender, args) } }
   on(Man.command)     { (sender, args) => replyTo(sender) { Man.handler(sender, args) } }
   on(Weather.command) { (sender, args) => replyTo(sender) { Weather.handler(sender, args) } }
-  on(Photo.command)   { (sender, args) => Photo.handler(sender, args)((photo, title) => sendPhoto(sender, photo, title), message => sendMessage(sender, message)) }
+  on(Photo.command)   { (sender, args) => Future { Photo.handler(sender, args)((photo, title) => sendPhoto(sender, photo, title), message => sendMessage(sender, message)) } }
 }
