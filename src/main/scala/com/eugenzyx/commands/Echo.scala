428a08c61cacdb6454044be2e27eef53607144ee
Mikhail Mezyakov <mihail265@gmail.com>
2016-03-18 12:50:40 +0300

    Prevent unwanted spaces

--- src/main/scala/com/eugenzyx/commands/Echo.scala
+++ src/main/scala/com/eugenzyx/commands/Echo.scala
@@ -4,8 +4,7 @@ import com.eugenzyx.commands.traits.Command
 
 object Echo extends Command {
   val command = "echo"
-  val description = """
-    Returns input text.
+  val description = """Returns input text.
     |Usage: /echo <text>
     |See also: hey""".stripMargin
 
